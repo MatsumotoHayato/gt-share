58ee678bc8399feec27b6d938970c633b19bb3e6
k458253 <k458253@kansai-u.ac.jp>
2022-02-14 18:00:06 +0900

    [change]PostsTableSeederの修正

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -14,8 +14,8 @@ class PostsTableSeeder extends Seeder
             ->each(function (Post $post) {
                 $users_list = range(1, \App\User::count());  // ユーザー数分の連番リスト
                 shuffle($users_list); // ランダムにいいねを生成するためシャッフル
-                $favorite_count_list = array(0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 4, 5)
-                $favorite_count = $favorite_count_list[array_rand($favorite_count_list, 1)]  // いいね数をランダムに決定
+                $favorite_count_list = array(0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 4, 5);
+                $favorite_count = $favorite_count_list[array_rand($favorite_count_list, 1)];  // いいね数をランダムに決定
                 $favorite_users = array_slice($users_list, 0, $favorite_count);  // リストの先頭からいいね数分ID取得
                 foreach($favorite_users as $user_id) {
                     $post->users()->attach($user_id);  // 中間テーブルに紐づけていいねをつける
